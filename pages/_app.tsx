90364503a6e324b084411140451cab292d44a6f1
Paul Kirby <paulrdkirby@gmail.com>
2022-04-08 15:45:38 -0500

    init commit

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import type { AppProps } from 'next/app'
+import { NextUIProvider, createTheme } from '@nextui-org/react'
+
+const darkTheme = createTheme({
+  type: 'dark',
+})
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <NextUIProvider theme={darkTheme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
+}
+
+export default MyApp
